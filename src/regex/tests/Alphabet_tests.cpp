6e2609715fa8ce4822b08d4bdc662afcfefc5955
Chris Guennewig <chrisguennewig@hotmail.com>
2022-11-25 15:19:38 -0500

    Restructure unit tests

---
+++ src/regex/tests/Alphabet_tests.cpp
@@ -0,0 +1,26 @@
+#include "catch.hpp"
+#include "Alphabet.hpp"
+
+namespace regex{
+
+namespace {
+
+SCENARIO( "Alphabet utilities")  
+{
+    SECTION("disjoin overlapping intervals in the alphabet #1")
+    {
+        auto myRanges = Alphabet{{2,2},{3,5}, {0,1}, {33,44},{33,37}, {31, 40}, {33,39}};
+        disjoinOverlap(myRanges, 0, 255);
+        CHECK((myRanges == Alphabet({{0,1}, {2,2}, {3,5}, {6,30}, {31,32}, {33,37}, {38,39}, {40,40}, {41,44}, {45, 255}})));
+    }
+
+    SECTION("disjoin overlapping intervals in the alphabet #2")
+    {
+        auto myRanges = Alphabet{{65,65},{124,124}, {124,124}, {66,66}, {255,255} ,{255,255}};
+        disjoinOverlap(myRanges, 0, 255);
+        CHECK((myRanges == Alphabet({{0,64}, {65,65}, {66,66}, {67,123}, {124,124}, {125,254}, {255,255}})));
+    }
+}
+
+} // namespace 
+} // namespace regex
---
+++ src/regex/tests/RegexSearch_tests.cpp
@@ -0,0 +1,55 @@
+#include "catch.hpp"
+#include "Regex.hpp"
+
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+
+
+namespace regex{
+
+namespace {
+
+
+SCENARIO( "Regex Search") 
+{
+
+/*
+    {
+        
+        auto regex = Regex("c");
+        regex.search("abcde");
+    }
+
+    {
+        
+        auto regex = Regex("abc");
+        regex.search("abcde");
+    }
+
+    {
+        
+        auto regex = Regex("abc");
+        regex.search("abcabcabc");
+    }
+
+    {
+        
+        auto regex = Regex("cat|hat");
+        regex.search("the cat has a hat");
+    }
+
+    {
+        
+        auto regex = Regex("cat|hat|h.s");
+        regex.search("the cat has a hat");
+    }
+
+*/
+
+}
+
+
+} // namespace 
+} // namespace regex
